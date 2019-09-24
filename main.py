87574b6bfa2edee9fae9408e4152d193dcd43d0c
eleutherius <eleutherius69@gmail.com>
2019-09-24 18:00:25 +0300

    add some changes

--- main.py
+++ main.py
@@ -19,12 +19,31 @@ def NameCounter (var):
 # print(NameCounter(test_list))
 
 """
+TASK #2
+
 Написать метод который принимет два числа a, b и возвращает все числа Фибоначчи на отрезке [a, b]
 """
-def Fibonachi (a, b):
+def Fibonacci (a, b):
+    out_list = []
+    fibonacci = []
+    for n in range (0, b):
+        if n == 0 :
+            fibonacci.append(0)
+        if n == 1:
+            fibonacci.append(1)
+        if n > 1:
+            fibonacci.append(fibonacci[n -1] + fibonacci[n -2])
+    for i in fibonacci:
+        if i >= a and i <= b:
 
+         out_list.append(i)
+    return out_list
+
+# print (Fibonacci(10, 100))
 
 """
+TASK #3
+
 получить список всех нечётных чисел от 0 до 100
 со звёздочкой - сделайте это в одну строку
 """
@@ -32,12 +51,77 @@ def Fibonachi (a, b):
 # print (a)
 
 """
+TASK #4
+
 напишите метод, который принимает на вход два параметра: a и b
 если тип обоих переменных (a и b) - int, вывести большее из них
 если тип обоих переменных строка - сообщить, является ли строка b подстрокой строки a
 если переменные разного типа, вывести сообщение об ошибке (любое)
 """
+def MyFunction (a, b):
+    if type(a) is int and type(b) is int:
+         if a > b:
+            print(a)
+         if a < b:
+            print (b)
+    elif type(a) is str and type(b) is str:
+        if b in a:
+             print ('b is sub string of a')
+        else:
+            print('b is not sub  string of a')
+    else:
+        print ('ERRR! differend types')
+
+# MyFunction ('hello', 'h')
+
+
 """
+TASK #5
+
 Напишіть функцію, яка приймає на вхід три параметри: початковий рік (a), кінцевий рік (b),
 список років (c). Функція має попертати список високосних років між а і b, крім вказаних у c
 """
+
+def IntercalaryYear(year):
+    if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
+        pass
+    else:
+        return year
+def Years (a,b,c):
+    MySET=set()
+    for i in range(a, b):
+        MySET.add(IntercalaryYear(i))
+    abc = MySET - set(c)
+    return abc
+c = [13, 12, 235, 1996]
+
+print (Years(1991, 2019, c))
+
+
+"""
+TASK #6
+
+Несколько задачек на массивы данных: 
+6.1
+- Найти сумму элементов массива
+6.2
+- Найти максимальный элемент, значение и индекс
+6.3
+- Найти минимальный элемент, значение и индекс
+6.4
+- Посчитать количество элементов больше нуля
+6.5
+- Прибавить к элементам массива их индекс
+6.6
+- Циклический сдвиг элементов массива на k- позиций вправо
+6.7
+- Вывести элементы одного массива, которые не равны элементам второго массива.
+6.8
+- Из двух отсортированных массивов сделать третий отсортированный, не сортируя его
+"""
+#6.1
+# a = [1, 2, 3]
+# b = sum (a)
+# print (b)
+
+#6.2
