2f24ef13d8ef83f31cd3cb5f367a6bc0b2303679
eleutherius <eleutherius69@gmail.com>
2020-12-07 21:56:29 +0200

    finished work with task4

--- tests/task4/task4.py
+++ tests/task4/task4.py
@@ -1,4 +1,5 @@
-from ..library import TestCase
+# from ..library import TestCase
+from ..library.common import DscMethod
 from testcase.testflow import executable
 from atl.utils.sequences import Namespace
 from ipmp.pages import GuiApi
@@ -6,6 +7,7 @@ import json
 from threading import Thread
 from atl.utils.sequences import Namespace
 from ipmp.emu.neo import NeoPanel
+import time
 
 _ALIAS_ = 'MyPanelsTests'
 
@@ -13,11 +15,12 @@ _ALIAS_ = 'MyPanelsTests'
 class Data(Namespace):
 
     def __init__(self):
-        self.group_name = "New group"
-        self.group_id: str
+        super(Data, self).__init__()
         self.psp_panel_id: str
-
-        pass
+        self.device_number = 1
+        self.psp_panel_serial = "C10070010101"
+        self.psp_fibro_account = "6999999999"
+        self.psp_system_account = "654321"
 
 
 """
@@ -28,24 +31,52 @@ class Data(Namespace):
 
 
 @executable(context=Data)
-class TestTask3_1(TestCase):
+class TestTask3_1(DscMethod):
 
     def __init__(self, data, *args, **kwargs):
         self.data = data
         super(TestTask3_1, self).__init__(*args, **kwargs)
+        self.neo = NeoPanel(self.data.psp_panel_serial, self.data.psp_fibro_account, 'IP', model='HS3128')
+        self.neo.config.host = self.connection.hostname
+
+    def Setup(self):
+        self.AssertLoginWasSuccess()
+
+    def Close(self):
+        serials = [self.neo.serial]
+        self.AssertPanelWasRemovedIfNeeded(serials)
 
     def EnrollPanel_Test(self):
-        self.AddMessage(f'Start test!')
-        neo = NeoPanel('A78899999999', '6999999999', 'IP', model='HS3128')
-        neo.config.version = '05.30'
-        neo.config.host = self.connection.hostname
-        for i in range(4):
-            neo.config.fibroReceivers[i]['host'] = '0.0.0.0'
-            neo.config.fibroReceivers[i]['hb'] = 0
-        neo.config.Eprom.refresh()
-        neo.add_device("contact", 1)
-        
-        thread = Thread(target=neo.connectITv2(connection_type="ip"))
+        self.neo.add_device("contact", self.data.device_number)
+        self.enroll_panel()
+        get_zone = self.get_device(self.data.psp_panel_serial, "ZONE", self.data.device_number)
+        self.AssertTrue(get_zone["subtype"] == "CONTACT", "Zone is not contact", f"Zone {self.data.device_number} is "
+                                                                                 f"contact")
+
+    def get_device(self, serial, device_type: str, device_number: int) -> dict:
+        unt_id = self.GuiApi.Units.getUnitId(serial)
+        return self.GuiApi.Diagnostic.getDevice(unt_id, device_type, device_number)
+
+    def EnrollPspGPRS_Panel_Test(self):
+        self.neo.setMedia("GSM")
+        self.enroll_panel()
+        self.AssertTrue(self.getStatusGsmUnit(self.neo.serial), "GSM module is not exist!", "GSM  module is exist")
+
+    def enroll_panel(self):
+        thread = Thread(target=self.neo.connectITv2)
         thread.start()
+        self.activate_neo_panel(panel=self.neo)
+        self.neo.sendInit()
+        self.neo.sendHeartBeat()
+        self.neo.config.version = ''
+        self.neo.stopITv2Session()
         thread.join()
-        self.AddMessage(f'Test finish!')
+
+    def GenerateEvents_Panel_Test(self):
+        self.enroll_panel()
+        self.GenerateAlarm(self.neo, alarm='PA')
+        self.GenerateAlarm(self.neo, alarm='PA')
+        # events = self.GuiApi.Events.getEvents()
+        time.sleep(3)
+        events_id = self.getIdEventsForPanel(self.neo.serial)
+        self.AssertTrue(len(events_id) == 2, "Events are not displayed",  "Events are displayed")
